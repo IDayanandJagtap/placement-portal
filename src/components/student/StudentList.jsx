fcffc2effbe6690459d773a932168e9077083005
Dayanand Jagtap <dayanandjagtap07@gmail.com>
2024-04-06 00:10:08 +0530

    Feat[search student]
    In student list file implement search student feature

--- src/components/student/StudentList.jsx
+++ src/components/student/StudentList.jsx
@@ -5,12 +5,28 @@ import {
     Input,
     InputRightElement,
     Grid,
+    Text,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { IoSearch } from "react-icons/io5";
 import StudentCard from "./StudentCard";
 
 const StudentList = ({ studentData }) => {
+    const [searchedStudents, setSearchedStudents] = useState(studentData);
+
+    const handleOnSearchInputChange = (e) => {
+        let searchable = e.target.value.toLowerCase();
+
+        if (searchable.trim() != "") {
+            let students = studentData.filter((element) => {
+                // make comparison of lowercase values
+                return element.name.toLowerCase().includes(searchable);
+            });
+            setSearchedStudents(students);
+        } else {
+            setSearchedStudents(studentData);
+        }
+    };
     return (
         <Stack p={[3, 4, 4, 4, 6]} rowGap={[4, 5, 5, 5, 6]} minH={"90vh"}>
             <HStack justifyContent={"center"}>
@@ -19,6 +35,7 @@ const StudentList = ({ studentData }) => {
                         type="text"
                         placeholder="Search student..."
                         variant={"outline"}
+                        onChange={handleOnSearchInputChange}
                         border={"2px solid #3182CE"}
                     />
                     <InputRightElement w={"4rem"}>
@@ -32,10 +49,21 @@ const StudentList = ({ studentData }) => {
                 overflow={"scroll"}
                 templateColumns={["1fr", "1fr", "1fr 1fr"]}
             >
-                {studentData.map((student) => {
+                {searchedStudents.map((student) => {
                     return <StudentCard key={student.id} student={student} />;
                 })}
             </Grid>
+
+            {searchedStudents.length == 0 && (
+                <Stack
+                    w={"full"}
+                    justifyContent={"center"}
+                    align={"center"}
+                    h={"full"}
+                >
+                    <Text>Searched student not found!</Text>
+                </Stack>
+            )}
         </Stack>
     );
 };
